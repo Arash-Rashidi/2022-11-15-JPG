8bb5f4c6ff4bc4e078ab45f30431f291c8c0ceba
Arash-Rashidi <arasheri@gmail.com>
2022-11-16 11:18:54 +0100

    Loading create page

--- js/nav.js
+++ js/nav.js
@@ -1,40 +1,60 @@
 function setNavbarEvent() {
-    document.getElementById('link-create').addEventListener('click', linkCreateEvt);
-    document.getElementById('link-thumb').addEventListener('click', linkThumbnailEvt);
-    document.getElementById('link-home').addEventListener('click', linkHomeEvt);
+    document
+        .getElementById("link-create")
+        .addEventListener("click", linkCreateEvt);
+    document
+        .getElementById("link-thumb")
+        .addEventListener("click", linkThumbnailEvt);
+    document.getElementById("link-home").addEventListener("click", linkHomeEvt);
 }
 /**
  * gestion des active de navbar
- * @param {object} evt event declencheur 
- * @param {boolean} setActiveparentli mettre le parent actif ou non 
+ * @param {object} evt event declencheur
+ * @param {boolean} setActiveparentli mettre le parent actif ou non
  */
 function setActiveLinkInNavbar(evt, setActiveparentli = true) {
-    var tousLesLi = document.querySelectorAll('nav>.navbar li');
+    var tousLesLi = document.querySelectorAll("nav>.navbar li");
     // for(var i=0;i<tousLesLi;i++){
     //     tousLesLi[i].classList.remove('active');
     // }
     tousLesLi.forEach(function (element) {
-        element.classList.remove('active');
-    })
-    if (setActiveparentli) { evt.target.parentElement.classList.add('active'); }
+        element.classList.remove("active");
+    });
+    if (setActiveparentli) {
+        evt.target.parentElement.classList.add("active");
+    }
 }
 function linkCreateEvt(evt) {
-    //echapement du comportement par defaut de la balise déclenchant l'evenement 
+    //echapement du comportement par defaut de la balise déclenchant l'evenement
     evt.preventDefault();
-    console.log('fonction liens create', evt);
+    console.log("fonction liens create", evt);
     setActiveLinkInNavbar(evt);
-
+    loadPage("create.html");
 }
 function linkHomeEvt(evt) {
-    //echapement du comportement par defaut de la balise déclenchant l'evenement 
+    //echapement du comportement par defaut de la balise déclenchant l'evenement
     evt.preventDefault();
-    console.log('fonction liens home', evt);
+    console.log("fonction liens home", evt);
     setActiveLinkInNavbar(evt, false);
-
 }
 function linkThumbnailEvt(evt) {
-    //echapement du comportement par defaut de la balise déclenchant l'evenement 
+    //echapement du comportement par defaut de la balise déclenchant l'evenement
     evt.preventDefault();
-    console.log('fonction liens thumbnail', evt);
+    console.log("fonction liens thumbnail", evt);
     setActiveLinkInNavbar(evt);
+}
+
+/**
+ *
+ * @param {string} pageHref file name  of the page of the wrapper
+ */
+function loadPage(pageHref) {
+    let pagePath = `/vues/${pageHref}`;
+    fetch(pagePath)
+        .then(function (resp) {
+            return resp.text();
+        })
+        .then(function (html) {
+            document.querySelector('#wrapper').innerHTML = html;
+        });
 }
