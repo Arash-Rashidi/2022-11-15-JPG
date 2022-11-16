7da04619545611753ade1153d9d3f851af8d2baa
Arash-Rashidi <arasheri@gmail.com>
2022-11-16 13:01:06 +0100

    correct nav bar functionality

--- js/nav.js
+++ js/nav.js
@@ -29,32 +29,55 @@ function linkCreateEvt(evt) {
     evt.preventDefault();
     console.log("fonction liens create", evt);
     setActiveLinkInNavbar(evt);
-    loadPage("create.html");
+    loadPage("create.html", (nodeBase) =>{
+        let form = nodeBase.querySelector('form');
+        form.addEventListener('submit', (evt) =>{
+            evt.preventDefault();
+            console.log('form sent');
+        })
+    });
 }
 function linkHomeEvt(evt) {
     //echapement du comportement par defaut de la balise déclenchant l'evenement
     evt.preventDefault();
     console.log("fonction liens home", evt);
     setActiveLinkInNavbar(evt, false);
+    loadPage('home.html');
 }
 function linkThumbnailEvt(evt) {
     //echapement du comportement par defaut de la balise déclenchant l'evenement
     evt.preventDefault();
     console.log("fonction liens thumbnail", evt);
     setActiveLinkInNavbar(evt);
+    loadPage('thumbnail.html');
 }
 
 /**
  *
  * @param {string} pageHref file name  of the page of the wrapper
+ * @param {Function} callback a call back function to add eventListener
  */
-function loadPage(pageHref) {
+function loadPage(pageHref, callback) {
     let pagePath = `/vues/${pageHref}`;
     fetch(pagePath)
         .then(function (resp) {
             return resp.text();
         })
         .then(function (html) {
-            document.querySelector('#wrapper').innerHTML = html;
+            let wrapperNode= document.querySelector('#wrapper');
+            wrapperNode.innerHTML = "";
+            /* let parser = new DOMParser();
+            let doc2 = parser.parseFromString(html, 'text/html');
+            console.log(doc2); */
+            let container = document.createElement('div');
+            container.innerHTML = html;
+            if (undefined !== callback && typeof(callback) === 'function' ){ // it is better to write like this in the condition
+                callback(container)
+            };
+            container.childNodes.forEach(element => {
+                wrapperNode.append(element);
+            });
+
+            return html;
         });
 }
