f133a333138bd10fca23ff9777e076a7cc1bae2f
Arash-Rashidi <arasheri@gmail.com>
2022-11-18 13:49:05 +0100

    loading image names in the option list

--- js6/formulaire.js
+++ js6/formulaire.js
@@ -10,22 +10,40 @@ export default class VueFormulaire{
     domRefElement = undefined;
     meme = undefined;
     #nodSelector
+    #container = undefined;
     constructor(nodSelector = '#wrapper'){
         this.#nodSelector = nodSelector;
     }
     #addEvents = () =>{}
-    #loadingContent = () =>{}
+    #loadingContent = () =>{
+        const select =  this.#container.querySelector('select');
+        //protection of multi execution of loading
+        select.innerHTML = '';
+        const noImg =document.createElement('option');
+        noImg.value = -1;
+        noImg.innerHTML = ' No Images';
+        select.append(noImg);
+        // loading all the options of images
+        images.map(e =>{
+            const option =document.createElement('option');
+            option.value = e.id;
+            option.innerHTML = e.titre;
+            select.append(option);
+
+        })
+
+    }
     loadPage = (domRefWrapper) =>{
         const promiseImage = Images.fetch(); 
         const promiseHTMLTemplate = loadPageByPromise(this.#vueHref);
         Promise.all([promiseHTMLTemplate, promiseImage])
         .then(arrayRessources => {
-            const container = arrayRessources[0];
+            this.#container = arrayRessources[0];
             images.replaceContentImagesArray(arrayRessources[1]);
             this.#loadingContent();
             this.domRefElement = document.querySelector(this.#nodSelector);
             this.domRefElement.innerHTML = '';
-            container.childNodes.forEach(elem => this.domRefElement.append(elem));
+            this.#container.childNodes.forEach(elem => this.domRefElement.append(elem));
         });
 
     }
